c40302bdcd876f637da34b1c5e270b78bc6b258a
DanielOv1103 <danieloliveirav1103@gmail.com>
2024-11-20 12:46:57 -0500

    feat: add feat types interfaces

--- src/modules/admin/components/prizes-list.tsx
+++ src/modules/admin/components/prizes-list.tsx
@@ -7,9 +7,9 @@ export default async function PrizesList() {
   const onlyShowPrizesWithImage = data.filter(prize => prize.imagen)
 
   return (
-    <main className="grid grid-cols-2 gap-4 w-full">
+    <main className="flex flex-col gap-4 w-full">
         <h2 className="text-2xl font-bold">Lista de premios</h2>
-        <div className="flex gap-4">
+        <div className="grid grid-cols-2 gap-4">
             {onlyShowPrizesWithImage.map((prize) => (
                 <div key={prize.id}>
                   <CardItem prize={prize} />
