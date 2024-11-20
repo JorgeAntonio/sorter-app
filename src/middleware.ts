21836ae4fa0937ea85aa67923565e0bbd9a26bae
JorgeAntonio <jorge.antonio.delaguila@gmail.com>
2024-11-20 11:38:51 -0500

    feat: update public routes for authentication and add 'use client' directive in sorter page

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,7 +5,7 @@ import { app_name } from './lib/constants'
  
 // 1. Specify protected and public routes
 const protectedRoutes = ['/admin']
-const publicRoutes = ['/login', '/signup', '/']
+const publicRoutes = ['/sign-in', '/signup', '/']
  
 export default async function middleware(req: NextRequest) {
   // 2. Check if the current route is protected or public
